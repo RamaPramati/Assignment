ca41f2d8f3f061b8e953c574dcf96bd143bb9d78
RamaPramati <ramakrishna.saparapu@imaginea.com>
2015-04-22 18:23:59 +0530

    Maven project

--- src/com/pramati/LineMatchFinder.java
+++ src/com/pramati/LineMatchFinder.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class MainClass {
+public class LineMatchFinder {
 
 	static Set<String> firstFileStrings = new HashSet<String>();
 	static Set<String> secondFileStrings = new HashSet<String>();
